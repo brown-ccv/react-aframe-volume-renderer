bdc950f0469c6345f0477fd66490a34e60f0da70
Kmilo9999 <k.diaz99@gmail.com>
2019-08-29 15:26:09 -0400

    importing aframe-webgl2 branch. moving aframe code to a react component

---
+++ src/components/VolumeRenderer.js
@@ -0,0 +1,37 @@
+
+
+import React, {Component} from 'react'
+
+
+
+export default class VolumeRenderer extends Component {
+  render () {
+    return (
+        <div className="aframe-container" > 
+          <a-scene embedded>
+            <a-box 
+              position="-1 0.5 -3" 
+              rotation="0 45 0" 
+              color="#4CC3D9" />
+            <a-sphere 
+              position="0 1.25 -5" 
+              radius="1.25" 
+              color="#EF2D5E" />
+            <a-cylinder 
+              position="1 0.75 -3" 
+              radius="0.5" 
+              height="1.5" 
+              color="#FFC65D" />
+            <a-plane 
+              position="0 0 -4" 
+              rotation="-90 0 0" 
+              width="4" 
+              height="4" 
+              color="#7BC8A4" />
+              <a-entity foo="bar: 5; baz: bazValue"></a-entity>
+          </a-scene>
+      </div>
+    );
+  }
+}
+
