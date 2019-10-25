094aae4f915a28e1c7bb48fbc5c6a3e3bcb8610b
Kmilo9999 <k.diaz99@gmail.com>
2019-10-25 14:47:38 -0400

    fixed opacity control. add, remove and edit points

--- src/components/OpacityControl.js
+++ src/components/OpacityControl.js
@@ -10,7 +10,7 @@ export default connect(
 
     constructor(props) {
         super(props);
-        this.lowNode = 0;
+        this.lowNode = 0.2;
         this.highNode = 1;
         this.minLevel = 0;
         this.maxLevel = 1;
@@ -37,13 +37,15 @@ export default connect(
 		this.minLevelY = ~~(this.height-(this.minLevel*this.height))+this.padding;
         this.maxLevelY = ~~(this.height-(this.maxLevel*this.height))+this.padding;
 
-        this.nodes =[{x:this.lowNodeX,y:this.minLevelY},{x:this.highNodeX,y:this.maxLevelY}];
+        this.nodes =[{x:10,y:0},{x:45,y:35},{x:81,y:21},{x:160,y:70}];
+        this.nodesCanvasSpace = [];
 
         this.changePointer = this.changePointer.bind(this);
         this.onMouseDown = this.onMouseDown.bind(this);
         this.onMouseUp = this.onMouseUp.bind(this);
         this.draggPointer = this.draggPointer.bind(this);
-        this.doubleClick = this.doubleClick.bind(this);
+        this.addPoint = this.addPoint.bind(this);
+        this.removePoint = this.removePoint.bind(this);
     }
 
 
@@ -54,7 +56,8 @@ export default connect(
         document.addEventListener("mousemove", this.draggPointer);
         this.opCanvas.addEventListener("mousedown",this.onMouseDown);
         document.addEventListener("mouseup", this.onMouseUp);
-        this.opCanvas.addEventListener("dblclick",this.doubleClick);
+        this.opCanvas.addEventListener("dblclick",this.addPoint);
+        this.opCanvas.addEventListener('contextmenu', this.removePoint);
     }
 
     componentWillUnmount() {
@@ -62,7 +65,8 @@ export default connect(
         document.removeEventListener("mousemove", this.draggPointer);
         this.opCanvas.removeEventListener("mousedown",this.onMouseDown);
         document.removeEventListener("mouseup", this.onMouseUp);
-        this.opCanvas.removeEventListener("dblclick",this.doubleClick);
+        this.opCanvas.removeEventListener("dblclick",this.addPoint);
+        this.opCanvas.removeEventListener('contextmenu', this.removePoint);
     }
 
     updateCanvas() {
@@ -86,77 +90,28 @@ export default connect(
 			
 		this.opContext.strokeStyle = "rgba(128, 128, 128, 0.8)";
 		this.opContext.lineWidth = 2;
-        //this.opContext.beginPath();
+       
+       this.nodesCanvasSpace = [];
+       for(var i = 0; i< this.nodes.length; i++)
+       { 
         
-        /*
-        var linesCount = 10;
-        for(var i = 0; i < linesCount; i++){
-            var y = 1-Math.pow(i/(linesCount-1), 2);
-            var y = this.padding + (this.height)*y;
-            y = ~~y+0.5;
-            this.opContext.moveTo(this.padding, y);
-            this.opContext.lineTo(this.width+this.padding, y);
-        }*/
-
+        var xPosInvertedCanvas = ~~(this.nodes[i].x)+ this.padding;
+        var yPosInvertedCanvas = ~~(this.height- this.nodes[i].y)+this.padding;
+        this.nodesCanvasSpace.push({x:xPosInvertedCanvas,y:yPosInvertedCanvas});
         
-        
-        /*this.opContext.stroke();
-			
-		this.opContext.strokeStyle = "#AAAAAA";
-		this.opContext.lineWidth = 2;
-		this.opContext.beginPath();
-		this.opContext.moveTo(this.padding, this.minLevelY);
-		this.opContext.lineTo(this.lowNodeX, this.minLevelY);
-		this.opContext.lineTo(this.highNodeX, this.maxLevelY);
-		this.opContext.lineTo(this.width+this.padding, this.maxLevelY);
-        this.opContext.stroke();
-        
-        if(this.hovering && this.nodeHovered == 0 || this.dragging && this.nodeDragged == 0){
-            this.opContext.fillStyle = "#FFFF55";
-        } else {
-            this.opContext.fillStyle = "#FFAA00";
-        }
-
-        this.opContext.beginPath();
-		this.opContext.arc(this.lowNodeX, this.minLevelY, 5, 0, 2*Math.PI);
-        this.opContext.fill();
-        
-        if(this.hovering && this.nodeHovered == 1 || this.dragging && this.nodeDragged == 1){
-            this.opContext.fillStyle = "#FFFF55";
-        } else {
-            this.opContext.fillStyle = "#FFAA00";
-        }
-
-        this.opContext.beginPath();
-		this.opContext.arc(this.highNodeX, this.maxLevelY, 5, 0, 2*Math.PI);
-        this.opContext.fill();*/
-        
-        //for(var i = 0; i< this.nodes.length; i++)
-        //{
-            this.nodes[0].x = ~~(this.width*this.nodes[0].x)+this.padding;
-            this.nodes[0].y = ~~(this.height-(this.nodes[0].y*this.height))+this.padding;
-
-            this.nodes[1].x = ~~(this.width*this.nodes[1].x)+this.padding;
-            this.nodes[1].y = ~~(this.height-(this.nodes[1].y*this.height))+this.padding;
-        //}
-
-
-        this.lowNodeX = ~~(this.width*this.lowNode)+this.padding;
-		this.highNodeX = ~~(this.width*this.highNode)+this.padding;
-		this.minLevelY = ~~(this.height-(this.minLevel*this.height))+this.padding;
-        this.maxLevelY = ~~(this.height-(this.maxLevel*this.height))+this.padding;
-        this.nodes =[{x:this.lowNodeX,y:this.minLevelY},{x:this.highNodeX,y:this.maxLevelY}];
-        if(this.nodes.length > 1 )
+       }
+     
+        if(this.nodesCanvasSpace.length > 1 )
         {
-          
+           // console.log("this.nodesCanvasSpace.length: " + this.nodesCanvasSpace.length);
             this.opContext.beginPath();
             this.opContext.moveTo(this.padding, this.minLevelY);
-            this.opContext.lineTo(this.nodes[0].x, this.nodes[0].y);
-            for(var i = 0; i <= this.nodes.length - 2; i++)
+            this.opContext.lineTo(this.nodesCanvasSpace[0].x, this.nodesCanvasSpace[0].y);
+            for(var i = 0; i <= this.nodesCanvasSpace.length - 2; i++)
             {
              
-                this.opContext.moveTo(this.nodes[i].x, this.nodes[i].y);
-                this.opContext.lineTo(this.nodes[i + 1].x, this.nodes[i + 1].y);
+                this.opContext.moveTo(this.nodesCanvasSpace[i].x, this.nodesCanvasSpace[i].y);
+                this.opContext.lineTo(this.nodesCanvasSpace[i + 1].x, this.nodesCanvasSpace[i + 1].y);
                 this.opContext.stroke();
             }
 
@@ -168,7 +123,7 @@ export default connect(
         this.opContext.strokeStyle = "#AAAAAA";
         this.opContext.lineWidth = 2;
        
-        for(var i = 0; i< this.nodes.length; i++)
+        for(var i = 0; i< this.nodesCanvasSpace.length; i++)
         {
             if(this.nodeHovered == i)
             {
@@ -179,20 +134,30 @@ export default connect(
                 this.opContext.fillStyle = "#FFAA00";
             }
             this.opContext.beginPath();
-            this.opContext.arc(this.nodes[i].x, this.nodes[i].y, 5, 0, 2*Math.PI);
+            this.opContext.arc(this.nodesCanvasSpace[i].x, this.nodesCanvasSpace[i].y, 5, 0, 2*Math.PI);
             this.opContext.fill();
         };
        
 			
     }
 
-    
- 
-    doubleClick(evt)
+    removePoint(evt)
     {
-     
-     var newPoint = {x: evt.offsetX, y: evt.offsetY}
-     
+      evt.preventDefault();
+      if(this.nodeHovered != -1 && this.nodeHovered != 0 &&  this.nodeHovered != this.nodes.length -1)
+      {
+        
+        this.nodes.splice(this.nodeHovered, 1);
+        this.nodeHovered = -1;
+      }
+    }
+ 
+    addPoint(evt)
+    {
+    
+    // insert points in canvas space
+     var newPoint = {x: evt.offsetX - this.padding , y: (this.height - evt.offsetY) + this.padding}
+     console.log("newPoint: " +newPoint.x + " "+newPoint.y)
 
      var indexToBeInserted = - 1;
      for(var i = 0; i< this.nodes.length; i++)
@@ -212,43 +177,25 @@ export default connect(
         this.nodes.splice(indexToBeInserted, 0, newPoint);
      }
      
+     for(var i = 0; i< this.nodes.length; i++)
+     {
+        console.log(this.nodes[i].x + " " + this.nodes[i].y)
+     }
+    
+    
+
      this.updateCanvas();
 
     }
 
     changePointer(e)
     {
-      
-        
-       /* if(Math.sqrt(Math.pow(e.offsetX-this.lowNodeX, 2)+Math.pow(e.offsetY-this.minLevelY, 2)) <= this.hoverRadius){
-          
-            if(!this.hovering){
-                this.opCanvas.className = "pointer";
-                this.nodeHovered = 0;
-                this.hovering = true;
-                this.updateCanvas();
-            }
-        } else if(Math.sqrt(Math.pow(e.offsetX-this.highNodeX, 2)+Math.pow(e.offsetY-this.maxLevelY, 2)) <= this.hoverRadius){
-      
-            if(!this.hovering){
-                this.opCanvas.className = "pointer";
-                this.nodeHovered = 1;
-                this.hovering = true;
-                this.updateCanvas();
-            }
-        } else {
-        
-            if(this.hovering){
-                this.opCanvas.className = "";
-                this.hovering = false;
-                this.updateCanvas();
-            }
-        }*/
-
         var hitPoint = false;
         for(var i = 0; i< this.nodes.length; i++)
         {
-            if(Math.sqrt(Math.pow(e.offsetX-this.nodes[i].x, 2)+Math.pow(e.offsetY-this.nodes[i].y, 2)) <= this.hoverRadius)
+            
+            var normalizedCoordinates = {x:  this.nodes[i].x + this.padding, y: (this.height- this.nodes[i].y)+this.padding };
+            if(Math.sqrt(Math.pow(e.offsetX-normalizedCoordinates.x, 2)+Math.pow(e.offsetY-normalizedCoordinates.y, 2)) <= this.hoverRadius)
             {
                 this.opCanvas.className = "pointer";
                 this.nodeHovered = i;
@@ -270,6 +217,7 @@ export default connect(
             }
         }
        
+       
 
     }
 
@@ -277,44 +225,14 @@ export default connect(
     {
         if(this.dragging){
             e.preventDefault();
-            var diffX = this.dragStart[0]-e.screenX;
-            var diffY = this.dragStart[1]-e.screenY;
+            var diffX = this.dragStart[0]- e.screenX;
+            var diffY = this.dragStart[1]- e.screenY;
             
-           /*if(this.nodeDragged == 0){
-                this.lowNodeX = Math.max(this.padding, Math.min(this.width+this.padding, this.startPos[0]-diffX));
-                this.minLevelY = Math.max(this.padding, Math.min(this.height+this.padding, this.startPos[1]-diffY));
-
-                this.lowNode = (this.lowNodeX-this.padding)/this.width;
-                this.lowNode = Math.min(this.lowNode, this.highNode);
-                this.minLevel = 1-(this.minLevelY-this.padding)/this.height;
-
-                this.props.myChangeLowNode(this.lowNode);
-                this.props.myChangePoint1(this.minLevel);
-               // console.log("minLevel: " +this.minLevel);
-            } else {
-                this.highNodeX = Math.max(this.padding, Math.min(this.width+this.padding, this.startPos[0]-diffX));
-                this.maxLevelY = Math.max(this.padding, Math.min(this.height+this.padding, this.startPos[1]-diffY));
-
-                this.highNode = (this.highNodeX-this.padding)/this.width;
-                this.highNode = Math.max(this.highNode, this.lowNode);
-                this.maxLevel = 1-(this.maxLevelY-this.padding)/this.height;
-
-                this.props.myChangeHighNode(this.highNode);
-                this.props.myChangePoint2(this.maxLevel);
-                //console.log("maxLevel " +this.maxLevel);
-            }*/
-
             if(this.nodeDragged != -1)
             {
-                this.nodes[this.nodeDragged].x = Math.max(this.padding, Math.min(this.width+this.padding, this.startPos[0]-diffX));
-                this.nodes[this.nodeDragged].y = Math.max(this.padding, Math.min(this.height+this.padding, this.startPos[1]-diffY));
+                this.nodes[this.nodeDragged].x = Math.max(this.minLevel, Math.min(this.width, this.startPos[0]-diffX));
+                this.nodes[this.nodeDragged].y = Math.max(this.minLevel, Math.min(this.height, this.startPos[1] +diffY));
 
-                //this.lowNode = (this.lowNodeX-this.padding)/this.width;
-               // this.lowNode = Math.min(this.lowNode, this.highNode);
-                //this.minLevel = 1-(this.minLevelY-this.padding)/this.height;
-
-               // this.props.myChangeLowNode(this.lowNode);
-              //  this.props.myChangePoint1(this.minLevel);
             }
             
             this.updateCanvas();
@@ -331,23 +249,10 @@ export default connect(
     onMouseDown(e)
     {
       
-       /* if(Math.sqrt(Math.pow(e.offsetX-this.lowNodeX, 2)+Math.pow(e.offsetY-this.minLevelY, 2)) <= this.hoverRadius){
-        
-            this.dragging = true;
-            this.nodeDragged = 0;
-            this.dragStart = [e.screenX, e.screenY];
-            this.startPos = [this.lowNodeX, this.minLevelY];
-        } else if(Math.sqrt(Math.pow(e.offsetX-this.highNodeX, 2)+Math.pow(e.offsetY-this.maxLevelY, 2)) <= this.hoverRadius){
-        
-            this.dragging = true;
-            this.nodeDragged = 1;
-            this.dragStart = [e.screenX, e.screenY];
-            this.startPos = [this.highNodeX, this.maxLevelY];
-        }*/
-        
         for(var i = 0; i< this.nodes.length; i++)
         {
-            if(Math.sqrt(Math.pow(e.offsetX-this.nodes[i].x, 2)+Math.pow(e.offsetY-this.nodes[i].y, 2)) <= this.hoverRadius)
+            var normalizedCoordinates = {x:  this.nodes[i].x + this.padding, y: (this.height- this.nodes[i].y)+this.padding };
+            if(Math.sqrt(Math.pow(e.offsetX-normalizedCoordinates.x, 2)+Math.pow(e.offsetY-normalizedCoordinates.y, 2)) <= this.hoverRadius)
             {
                 this.dragging = true;
                 this.nodeDragged = i;
