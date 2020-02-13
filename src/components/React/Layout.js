9ed1785a2fce561ae4e1d7353bc3d5952a8e8f41
Kmilo9999 <k.diaz99@gmail.com>
2020-02-12 23:04:59 -0500

    Fixed info color and style. Fixed  people email and github link color

--- src/components/React/Layout.js
+++ src/components/React/Layout.js
@@ -73,25 +73,36 @@ export default class Layout extends Component {
               <p>Web VR Volume Renderer</p>
             </div>
             
-            <p className="light-page-description">
-            The Center for Computation and Visulization at Brown University (<a href="https://ccv.brown.edu/">CCV</a>)
-is always searching and developing tools to help researchers visualizating and analizing their data.
-Thinking on how to facilitate the access to scienctific data from any type of device and location, and using
-the latests techonologies on web development, web 3D rendering and Virtual Reality (VR), this application
-is presented as a an initative to address those goals.<br/>
-</p>
-<p className="light-page-description">
-This page demonstrates a VR-capable 3D volume visualization tool built
-on top of webgl and <a href="https://aframe.io/">aframe</a>. The main
-goal of the project is to provide an easily accessible and interactive
-environment to explore and showcase volumetric data with the added
-benefits of Virtual Reality if VR-capable hardware is available. The
-Center for Computation and Visualization at Brown University  (<a
-href="https://ccv.brown.edu/">CCV</a>) encourages researchers from any
-field and background interested in using this application to contact us
-about how it can be used for your research projects.<br/>
-</p>
-<p className="light-page-description">
+            <div className="light-page-description">
+
+            <p>
+              The Center for Computation and Visulization at Brown University (<a href="https://ccv.brown.edu/">CCV</a>)
+              is always searching and developing tools to help researchers visualizating and analizing their data.
+              Thinking on how to facilitate the access to scienctific data from any type of device and location, and using
+              the latests techonologies on web development, web 3D rendering and Virtual Reality (VR), this application
+             is presented as a an initative to address those goals.<br/>
+              </p>
+            </div>
+            
+            <div className="light-page-description">
+              <p>
+                This page demonstrates a VR-capable 3D volume visualization tool built
+                on top of webgl and <a href="https://aframe.io/">aframe</a>. The main
+                goal of the project is to provide an easily accessible and interactive
+                environment to explore and showcase volumetric data with the added
+                benefits of Virtual Reality if VR-capable hardware is available. The
+                Center for Computation and Visualization at Brown University  (<a
+                href="https://ccv.brown.edu/">CCV</a>) encourages researchers from any
+                field and background interested in using this application to contact us
+                about how it can be used for your research projects.<br/>
+              </p>
+
+
+            </div>
+
+            <div className="light-page-description">
+
+            <p>
 CCV's goal is to help researchers in visualizing and analyzing their
 data and provides tools which facilitate the access to scientific data
 from any type of device and location by using the latest technologies on
@@ -99,11 +110,14 @@ web development, 3D web rendering and Virtual Reality (VR). With over 5
 years of experience, it has assisted researchers in projects at Brown
 University and affiliated RI institutions.<br/>
 </p>
-<p className="light-page-description">
+<p>
 This work was supported by the NSF EPSCoR grant 1655221: "RII Track-1:
 Rhode Island Consortium for Coastal Ecology Assessment, Innovation, and
 Modeling (C-AIM)"<br/>
 </p>
+            </div>
+
+
             
           </div>
         </Element>
@@ -118,9 +132,9 @@ Modeling (C-AIM)"<br/>
                 <Flexbox element="div" justifyContent="center" height="60px" width="33%">
                   <Flexbox display="flex" flexDirection="column">
                     <img src={Camilo} height="200px"></img>
-                    <div>
-                      <a href="mailto:camilo_diaz@brown.edu?Subject=Interest in AFrame Viewer" target="_top">Camilo Diaz</a>
-                      <br /><a href="https://github.com/kmilo9999" target="_blank"><Github width={30} /></a>
+                    <div >
+                      <a className="peopleLink" href="mailto:camilo_diaz@brown.edu?Subject=Interest in AFrame Viewer" target="_top">Camilo Diaz</a>
+                      <br /><a className="peopleLink" href="https://github.com/kmilo9999" target="_blank"><Github width={30} /></a>
                     </div>
                   </Flexbox>
                 </Flexbox>
@@ -128,8 +142,8 @@ Modeling (C-AIM)"<br/>
                   <Flexbox display="flex" flexDirection="column">
                     <img src={Ben} height="200px"></img>
                     <div>
-                      <a href="mailto:benjamin_knorlein@brown.edu?Subject=Interest in AFrame Viewer" target="_top">Benjamin Knörlein</a>
-                      <br /><a href="https://github.com/BenKnorlein" target="_blank"><Github width={30} /></a>
+                      <a className="peopleLink" href="mailto:benjamin_knorlein@brown.edu?Subject=Interest in AFrame Viewer" target="_top">Benjamin Knörlein</a>
+                      <br /><a className="peopleLink" href="https://github.com/BenKnorlein" target="_blank"><Github width={30} /></a>
                     </div>
                   </Flexbox>
                 </Flexbox>
@@ -137,8 +151,8 @@ Modeling (C-AIM)"<br/>
                   <Flexbox display="flex" flexDirection="column">
                     <img src={Kalvin} height="200px"></img>
                     <div>
-                      <a href="mailto:kalvin_lam@brown.edu?Subject=Interest in AFrame Viewer" target="_top">Kalvin Lam</a>
-                      <br /><a href="https://github.com/theklam" target="_blank"><Github width={30} /></a>
+                      <a className="peopleLink" href="mailto:kalvin_lam@brown.edu?Subject=Interest in AFrame Viewer" target="_top">Kalvin Lam</a>
+                      <br /><a className="peopleLink" href="https://github.com/theklam" target="_blank"><Github width={30} /></a>
                     </div>
                   </Flexbox>
                 </Flexbox>
