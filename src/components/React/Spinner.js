55d994ccd1aa7ec287769636a87835f8271d4904
RobertGemmaJr <rgemma.jr@gmail.com>
2021-09-14 16:44:10 -0400

    fix(Spinner.js): fix props issue with function component

--- src/components/React/Spinner.js
+++ src/components/React/Spinner.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { RingLoader } from 'react-spinners';
 
-export default function Spinner() {
+export default function Spinner(props) {
   return (
     <div className="sweet-loading row justify-content-md-center">
       <div className="col-md-auto my-4">
         <RingLoader
           color={"#3B5B8C"}
-          loading={this.props.loading}
-          size={this.props.size || 50}
+          loading={props.loading}
+          size={props.size || 50}
         />
       </div>
     </div>
