52116320b9c92f57c028ceda3e2b79a88166b06b
RobertGemmaJr <rgemma.jr@gmail.com>
2021-09-20 11:32:55 -0400

    feat: control <ToggleButtons> for season and tide
    Changed ciomponent to function based

--- src/components/React/ControlsPanel.js
+++ src/components/React/ControlsPanel.js
@@ -1,93 +1,60 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import React, {useState} from 'react';
 import {Sidebar} from 'primereact/sidebar';
-import {SelectButton} from 'primereact/selectbutton';
-
-import {myChangeVolumeAction} from '../../redux/AppActions'
-import Controls from './Controls';
 import { Col, Row, Button, Container, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
+import Controls from './Controls';
 
-// const options = [
-//   {value: './assets/models/48hr_20x_23_0.597976_ 0.597976_5.png:false', label: 'Spheroid old' },
-//   {value: './assets/models/r06c03f04_49_1.29_1.29_5.00000.png:false', label: 'Spheroid 1 cropped slices' },
-//   {value: './assets/models/r06c03f04_90_1.935_1.935_5.00000.png:false', label: 'Spheroid 1 90 slices' },
-//   {value: './assets/models/r06c03f03_49_1.29_1.29_5.00000.png:false', label: 'Spheroid 2 cropped slices' },
-//   {value: './assets/models/r06c03f03_90_1.935_1.935_5.00000.png:false', label: 'Spheroid 2 90 slices' },
-// ];
+export default function ControlPanel(props) {
 
-export default connect(null, {myChangeVolumeAction})
-( class ControlPanel extends Component{
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const [volume, setVolume] = useState({
+    season: 0, 
+    tide: 1
+  });
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      sideBarVisible: false,
-      currentData: null,
-      currentVolume:"",
-    };
-    this.volumeSelectChanged = this.volumeSelectChanged.bind(this);
-  }
+  return (
+    <Container fluid className="my-3">
+      <Row>
+        <Col className="text-center">
+          <Button onClick={(e) => setSidebarVisible(!sidebarVisible)}>
+            Options
+          </Button>
+        </Col>
 
-  volumeSelectChanged = (selected) => {
-    this.setState({
-      currentVolume: selected.value,
-      currentData:selected
-    });
-    
-    if(selected.value != null) {
-      var volumeProperties = selected.value.split(":");
-      this.props.myChangeVolumeAction(volumeProperties[0], volumeProperties[1]);
-    } else {
-      this.props.myChangeVolumeAction("", "");
-    }
-  };
+        <Col className="text-center">
+          <ToggleButtonGroup 
+            type="radio" 
+            name="season" 
+            value={volume.season} 
+            onChange={(val) => setVolume({...volume, season: val})}
+          >
+            <ToggleButton value={0}>Summer</ToggleButton>
+            <ToggleButton value={1}>Winter</ToggleButton>
+          </ToggleButtonGroup>
 
-  render () {
-    return (
-      <Container fluid className="my-3">
-        <Row>
-          <Col className="text-center">
-            <Button 
-              variant="outline-primary"
-              active
-              onClick={(e) => this.setState({sideBarVisible: true})}
-            >
-              Options
-            </Button>
-          </Col>
+        </Col>
 
-          <Col className="text-center">
-            <ToggleButtonGroup type="radio" name="season" value={1}>
-              <ToggleButton value={1}>Summer</ToggleButton>
-              <ToggleButton value={2}>Winter</ToggleButton>
-            </ToggleButtonGroup>
+        <Col className="text-center">
+          <ToggleButtonGroup 
+            type="radio" 
+            name="tide"
+            value={volume.tide}
+            onChange={(val) => setVolume({...volume, tide: val})}
+          >
+            <ToggleButton value={0}>Low Tide</ToggleButton>
+            <ToggleButton value={1}>High Tide</ToggleButton>
+          </ToggleButtonGroup>
+        </Col>
+      </Row>
 
-          </Col>
-
-          <Col className="text-center">
-            <ToggleButtonGroup type="radio" name="season" value={2}>
-              <ToggleButton value={1}>Temperature</ToggleButton>
-              <ToggleButton value={2}>Salinity</ToggleButton>
-            </ToggleButtonGroup>
-          </Col>
-
-          {/* Model Section TO BE REMOVED */}
-          {/* <SelectButton 
-            value={this.state.currentVolume}
-            options={options} 
-            onChange={this.volumeSelectChanged} 
-          />  */}
-        </Row>
-        <Sidebar
-          modal={false}
-          position="bottom"
-          visible={this.state.sideBarVisible} 
-          onHide={(e) => this.setState({sideBarVisible:false})}
-          style={{width:'20em', height:'45em'}}
-        >
-          <Controls/>
-        </Sidebar>
-      </Container>
-    );
-  }
-})
+      <Sidebar
+        modal={false}
+        position="bottom"
+        visible={sidebarVisible} 
+        onHide={(e) => setSidebarVisible(false)}
+        style={{width:'20em', height:'45em'}}
+      >
+        <Controls/>
+      </Sidebar>
+    </Container>
+  );
+}
