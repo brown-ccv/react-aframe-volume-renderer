a102e62d107062737a8ec987a81dfb7fff0a3821
RobertGemmaJr <rgemma.jr@gmail.com>
2021-09-20 10:31:37 -0400

    feat: use ToggleButtons for model selection
    ToggleButtons are not yet controlled

--- src/components/React/ControlsPanel.js
+++ src/components/React/ControlsPanel.js
@@ -1,20 +1,19 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import {Sidebar} from 'primereact/sidebar';
-// import {Button} from 'primereact/button';
 import {SelectButton} from 'primereact/selectbutton';
 
 import {myChangeVolumeAction} from '../../redux/AppActions'
 import Controls from './Controls';
-import { Col, Row, Button, Container, Form } from 'react-bootstrap';
+import { Col, Row, Button, Container, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 
-const options = [
-  {value: './assets/models/48hr_20x_23_0.597976_ 0.597976_5.png:false', label: 'Spheroid old' },
-  {value: './assets/models/r06c03f04_49_1.29_1.29_5.00000.png:false', label: 'Spheroid 1 cropped slices' },
-  {value: './assets/models/r06c03f04_90_1.935_1.935_5.00000.png:false', label: 'Spheroid 1 90 slices' },
-  {value: './assets/models/r06c03f03_49_1.29_1.29_5.00000.png:false', label: 'Spheroid 2 cropped slices' },
-  {value: './assets/models/r06c03f03_90_1.935_1.935_5.00000.png:false', label: 'Spheroid 2 90 slices' },
-];
+// const options = [
+//   {value: './assets/models/48hr_20x_23_0.597976_ 0.597976_5.png:false', label: 'Spheroid old' },
+//   {value: './assets/models/r06c03f04_49_1.29_1.29_5.00000.png:false', label: 'Spheroid 1 cropped slices' },
+//   {value: './assets/models/r06c03f04_90_1.935_1.935_5.00000.png:false', label: 'Spheroid 1 90 slices' },
+//   {value: './assets/models/r06c03f03_49_1.29_1.29_5.00000.png:false', label: 'Spheroid 2 cropped slices' },
+//   {value: './assets/models/r06c03f03_90_1.935_1.935_5.00000.png:false', label: 'Spheroid 2 90 slices' },
+// ];
 
 export default connect(null, {myChangeVolumeAction})
 ( class ControlPanel extends Component{
@@ -37,7 +36,7 @@ export default connect(null, {myChangeVolumeAction})
     
     if(selected.value != null) {
       var volumeProperties = selected.value.split(":");
-      this.props.myChangeVolumeAction(volumeProperties[0],volumeProperties[1]);
+      this.props.myChangeVolumeAction(volumeProperties[0], volumeProperties[1]);
     } else {
       this.props.myChangeVolumeAction("", "");
     }
@@ -53,39 +52,34 @@ export default connect(null, {myChangeVolumeAction})
               active
               onClick={(e) => this.setState({sideBarVisible: true})}
             >
-              Controls
+              Options
             </Button>
           </Col>
 
-          <Col>
-            <Form.Group>
-              <Form.Label as="legend" className="text-center">
-                Season
-              </Form.Label>
-              <Form.Check type="radio" label="Summer" />
-              <Form.Check type="radio" label="Winter" />
-            </Form.Group>
+          <Col className="text-center">
+            <ToggleButtonGroup type="radio" name="season" value={1}>
+              <ToggleButton value={1}>Summer</ToggleButton>
+              <ToggleButton value={2}>Winter</ToggleButton>
+            </ToggleButtonGroup>
+
           </Col>
 
-          <Col>
-            <Form.Group>
-              <Form.Label as="legend" className="text-center">
-                Tide
-              </Form.Label>
-              <Form.Check type="radio" label="High Tide" />
-              <Form.Check type="radio" label="Low Tide" />
-            </Form.Group>
+          <Col className="text-center">
+            <ToggleButtonGroup type="radio" name="season" value={2}>
+              <ToggleButton value={1}>Temperature</ToggleButton>
+              <ToggleButton value={2}>Salinity</ToggleButton>
+            </ToggleButtonGroup>
           </Col>
 
           {/* Model Section TO BE REMOVED */}
-          <SelectButton 
+          {/* <SelectButton 
             value={this.state.currentVolume}
             options={options} 
             onChange={this.volumeSelectChanged} 
-          /> 
+          />  */}
         </Row>
         <Sidebar
-          modal={false} 
+          modal={false}
           position="bottom"
           visible={this.state.sideBarVisible} 
           onHide={(e) => this.setState({sideBarVisible:false})}
